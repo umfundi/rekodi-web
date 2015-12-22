f88709d5181fb84c7709dc258f5ea7a91eb1b3b8
Umfundi <ian.molesworth@umfundi.com>
2015-12-22 17:34:27 +0000

    Initial commit of framework site

---
+++ frontend/views/site/index.php
@@ -0,0 +1,52 @@
+<?php
+
+/* @var $this yii\web\View */ 
+
+$this->title = 'Rekodi';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Welcome to Rekodi!</h1>
+
+        <p class="lead">A revolution in scoring and records integration!</p>
+
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+           <div class="col-lg-4">
+                <h2>Records</h2>
+
+                <p>Our records system delivers a complete end-to-end records solution. Define your own records structure or use 
+                	one of the standard ones we provide to get you up-and-running in no time. From simple organisational data for 
+                	your club, school or institution to fully detailed records of acheivements on the sporting field.
+                	 Capture and then Integrate your stored data back into your own site or social media page.</p>
+
+               <p><a class="btn btn-default" href="/frontend/web/index.php?r=site%2Fsignup">Signup &raquo;</a></p>
+             </div>
+            <div class="col-lg-4">
+                <h2>Scoring</h2>
+
+                <p>Our system gives you the only complete end-to-end scoring solution. Leveraging all of the latest innovations, we 
+                	provide scoring right from the field of competition via your office and through to the web.  Our servers provide a simple 'API' which allows you to 
+                	integrate your stored data back into your own website. We provide plugins for CMS systems that allow the most
+                	modest webmasters to pRovide a professional presentation page or pages on your site or social media page.</p>
+
+                <p><a class="btn btn-default" href="/frontend/web/index.php?r=site%2Fsignup">Signup &raquo;</a></p>
+            </div>
+             <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="/frontend/web/index.php?r=site%2Fsignup">Signup &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
---
+++ frontend/views/site/userpage.php
@@ -0,0 +1,64 @@
+<?php
+
+/* @var $this yii\web\View */ 
+
+$this->title = 'Rekodi';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Your Rekodi profile</h1>
+
+    </div>
+    
+    profile information
+    		Organization
+    		
+    		
+	Integrations
+	
+    
+    
+    Scoring systems
+    		Clay pigeon shooting
+    			Sporting
+    			Trap
+    			
+		
+    
+
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Scoring</h2>
+
+                <p>Our system offers the only complete end-to-end scoring solution. leveraging all of the latest innovations we 
+                	provide scoring right from the field of competition to the web.  </p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
